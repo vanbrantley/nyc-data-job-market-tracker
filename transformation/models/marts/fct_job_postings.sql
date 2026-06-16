0ff5050e213e615ba42dc000b3a99f2bfdcc944c
Van Brantley <van.brantley@gmail.com>
2026-06-16 19:10:52 -0400

    update transformation models to handle new enrichment fields

--- transformation/models/marts/fct_job_postings.sql
+++ transformation/models/marts/fct_job_postings.sql
@@ -2,39 +2,4 @@ with int_jobs as (
     select * from {{ ref('int_jobs_unioned') }}
 )
 
-select
-    job_id,
-    source,
-    ingestion_query,
-    job_title,
-    company_name,
-    job_url,
-    date_posted,
-    description,
-    city,
-    state,
-    country,
-    latitude,
-    longitude,
-    work_model,
-    employment_type,
-    listed_seniority,
-    is_explicitly_entry_level,
-    final_salary_min,
-    final_salary_max,
-    inferred_seniority,
-    role_archetype,
-    work_focus,
-    is_title_inflated,
-    inflation_reasoning,
-    tech_stack_required,
-    tech_stack_preferred,
-    paradigms_required,
-    paradigms_preferred,
-    degree_requirement,
-    years_required_min,
-    years_required_max,
-    confidence_score,
-    enriched_at,
-    ingested_at
-from int_jobs
+select * from int_jobs
